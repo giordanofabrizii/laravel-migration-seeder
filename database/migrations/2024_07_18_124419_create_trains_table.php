1957ef75c0767058fbe1143f0fd2ea4340a4623a
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-18 15:13:46 +0200

    added migration

---
+++ database/migrations/2024_07_18_124419_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string("Azienda",30);
+            $table->string("Stazione_di_partenza",30);
+            $table->string("Stazione_di_arrivo",30);
+            $table->date("Orario_di_partenza");
+            $table->date("Orario_di_arrivo");
+            $table->string("Codice_treno", 15);
+            $table->tinyInteger("Numero_carrozze")->unsigned();
+            $table->boolean("In_orario")->default(true);
+            $table->boolean("Cancellato")->default(false);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
